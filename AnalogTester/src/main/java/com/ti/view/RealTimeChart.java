94851bcdce835c8b9cd3ec13ce8e924a580c1d02
Aleksey <tihiy-ti@mail.ru>
2017-06-02 11:17:18 +0300

    Release 0.2.5 Добавлено автомасштабирование.
    Note:
    Пресобрано на jdk1.8.0_05, на 121 гораздо больше тормозов, не стал разбираться в чем дело... просто собрал на старой версии
    При запуске в IDEA до сборки MAX ~5000
    Собрано Gradle на jdk1.8.0_05        ~3000
    Собрано Gradle на jdk1.8.0_121      ~1000

--- AnalogTester/src/main/java/com/ti/view/RealTimeChart.java
+++ AnalogTester/src/main/java/com/ti/view/RealTimeChart.java
@@ -2,6 +2,8 @@ package com.ti.view;
 
 import com.ti.PropertiesService;
 import javafx.animation.AnimationTimer;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
@@ -9,7 +11,10 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Polyline;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class RealTimeChart{
@@ -17,8 +22,9 @@ public class RealTimeChart{
     private int maxDataPoint = 1000;
     private boolean skip10 = true;
 
-    private ConcurrentLinkedQueue<Number> dataQ = new ConcurrentLinkedQueue<Number>();
-    private ConcurrentLinkedQueue<Number> dataOfMax = new ConcurrentLinkedQueue<Number>();
+    private ConcurrentLinkedQueue<Integer> dataQ = new ConcurrentLinkedQueue<>();
+    private List<Integer> tempList = new ArrayList<>();
+    private ConcurrentLinkedQueue<Integer> dataOfMax = new ConcurrentLinkedQueue<>();
     private LineChartWithMarkers<Number, Number> sc;
     private NumberAxis xAxis;
     private NumberAxis yAxis;
@@ -35,6 +41,7 @@ public class RealTimeChart{
 //        xAxis.setScaleX(1/4);
         yAxis = new NumberAxis();
         yAxis.setAutoRanging(true);
+        yAxis.setForceZeroInRange(false);
         series = new AreaChart.Series<Number,Number>();
         series.setName("Area Chart Series");
 
@@ -71,7 +78,9 @@ public class RealTimeChart{
             if (dataQ.isEmpty()) break;
             xSeriesData = xSeriesData+1;
             if((xSeriesData%10) == 0){
-                series.getData().add(new LineChart.Data(xSeriesData/10, dataQ.remove()));
+                int addingValue = dataQ.remove();
+                tempList.add(addingValue);
+                series.getData().add(new LineChart.Data(xSeriesData/10, addingValue));
             }else{
                 dataQ.remove();
             }
@@ -89,32 +98,49 @@ public class RealTimeChart{
         // update
         xAxis.setLowerBound((xSeriesData/10-maxDataPoint));
         xAxis.setUpperBound((xSeriesData)/10-1);
+
+//        Integer min = Collections.min(dataQ);
+//        Integer max = Collections.max(dataQ);
+//        if(min!=null){
+//            yAxis.setLowerBound(min);
+//            yAxis.setUpperBound(max);
+//        }
     }
     private void addDataToSeriesAll() {
-//        System.out.println("Data size " + dataQ.size() + new Date().toString());
-
-//        System.out.print(" s:"+dataQ.size()+" ");
         for (int i = 0; i < dataQ.size(); i++) { //-- add 20 numbers to the plot+
-//            if (dataQ.isEmpty()) break;
             xSeriesData = xSeriesData+1;
-//            if((xSeriesData%10) == 0){
-                series.getData().add(new LineChart.Data(xSeriesData, dataQ.remove()));
-//            }else{
-//                dataQ.remove();
-//            }
-//            while(dataOfMax.size()>0){
-//                int maximum = (Integer)dataOfMax.poll();
-//                System.out.println("Add maximum " + maximum);
-//                sc.addVerticalValueMarker(new XYChart.Data<>(maximum/10, 0 ));
-//            }
+            int addingValue = dataQ.remove();
+            tempList.add(addingValue);
+            if(tempList.size() > maxDataPoint){
+                tempList.remove(0);
+            }
+            series.getData().add(new LineChart.Data(xSeriesData, addingValue));
         }
-        // remove points to keep us at no more than MAX_DATA_POINTS
         if (series.getData().size() > maxDataPoint) {
             series.getData().remove(0, series.getData().size() - maxDataPoint);
         }
-        // update
         xAxis.setLowerBound((xSeriesData - maxDataPoint));
         xAxis.setUpperBound((xSeriesData));
+
+//        if(!dataQ.isEmpty()){
+//            Integer min = Collections.min(tempList);
+//            Integer max = Collections.max(tempList);
+//            int from = (int) yAxis.getLowerBound();
+//            int to = (int) yAxis.getUpperBound();
+//            System.out.println("from = "+from + "  to = "+ to +"   "+ min+ "   "+max);
+//            if(min!=null){
+//                if( min < (from + 100) && min > from - 100){
+//
+//                }else {
+//                    yAxis.setLowerBound(min - 100);
+//                }
+//                if( max < (to + 100) && max > to - 100){
+//
+//                }else {
+//                    yAxis.setUpperBound(max + 100);
+//                }
+//            }
+//        }
     }
 
     public void setQueue(ConcurrentLinkedQueue queue){
